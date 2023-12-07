9903cfe76b6baff18eefe77ffd0ba29ddb0856dd
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-12-07 21:20:45 +0100

    Prepare for 0.02 version

--- ToFail.pm
+++ ToFail.pm
@@ -21,7 +21,7 @@ Readonly::Hash our %PRESETS => (
 	],
 );
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -182,6 +182,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
